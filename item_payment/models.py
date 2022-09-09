bfd30d9b0a99227a6739e2b42dd83d4cbaa5c63b
Den4ik-Bro <denis.rr@bk.ru>
2022-09-09 19:51:47 +0300

    add Item model

--- item_payment/models.py
+++ item_payment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(blank=True, verbose_name='описание')
+    price = models.IntegerField(default=0, verbose_name='цена')
+    stripe_product_id = models.CharField(max_length=100)
+    stripe_price_id = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
