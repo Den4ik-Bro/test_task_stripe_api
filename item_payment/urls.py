29695dca3a241203fa509c75db5c2d4472bc6be1
Den4ik-Bro <denis.rr@bk.ru>
2022-09-09 21:25:39 +0300

    work on app, add new views, urls.path, new templates

--- item_payment/urls.py
+++ item_payment/urls.py
@@ -6,4 +6,7 @@ app_name = 'item_payment'
 
 urlpatterns = [
     path('item/<int:pk>/', views.ItemDetailView.as_view(), name='item'),
+    path('create_checkout_session/<int:pk>/', views.CreateCheckoutSessionView.as_view(), name='create_session'),
+    path('success/', views.SuccessView.as_view(), name='success'),
+    path('cancel/', views.CancelView.as_view(), name='success'),
 ]
--- item_payment/views.py
+++ item_payment/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+import stripe
+from django.shortcuts import redirect
+from django.views import View
+from django.views.generic import DetailView, TemplateView
 from .models import Item
+from core import settings
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class ItemDetailView(DetailView):
     queryset = Item.objects.all()
     template_name = 'item_payment/item.html'
+
+
+class CreateCheckoutSessionView(View):
+
+    def post(self, request, *args, **kwargs):
+        item = Item.objects.get(id=self.kwargs['pk'])
+        url = 'http://127.0.0.1:8000'
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price': item.stripe_price_id,
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=url + '/success/',
+            cancel_url=url + '/cancel/',
+        )
+        return redirect(checkout_session.url)
+
+
+class SuccessView(TemplateView):
+    template_name = 'item_payment/success.html'
+
+
+class CancelView(TemplateView):
+    template_name = 'item_payment/cancel.html'
